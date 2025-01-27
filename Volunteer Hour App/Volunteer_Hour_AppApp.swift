4d29dd713aac91ae1b505517a0064f9a05c30ada
JonathanVarghese24 <jonathanvarghese24@gmail.com>
2025-01-27 09:23:38 -0600

    Initial Commit

---
+++ Volunteer Hour App/Volunteer_Hour_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volunteer_Hour_AppApp.swift
+//  Volunteer Hour App
+//
+//  Created by JV on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Volunteer_Hour_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Volunteer Hour AppTests/Volunteer_Hour_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Volunteer_Hour_AppTests.swift
+//  Volunteer Hour AppTests
+//
+//  Created by JV on 1/27/25.
+//
+
+import Testing
+@testable import Volunteer_Hour_App
+
+struct Volunteer_Hour_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
